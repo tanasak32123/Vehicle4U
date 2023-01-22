7e5f9045d4dd3f46887200b69f24d2cbea29123b
Tanasak Pusawatwong <tanasak32123@gmail.com>
2023-01-23 01:16:05 +0700

    first commit

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    // if you have other folders, add their paths too, like this:
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
