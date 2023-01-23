a7ab930c78dcae818c8f2108e335236df23b5a08
Tanasak Pusawatwong <tanasak32123@gmail.com>
2023-01-23 20:31:55 +0700

    create navbar

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -2,15 +2,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import "bootstrap/dist/css/bootstrap.css";
+import { SSRProvider } from "react-bootstrap";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SSRProvider>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
       <Component {...pageProps} />
-    </>
+    </SSRProvider>
   );
 }
