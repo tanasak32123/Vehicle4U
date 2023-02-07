87a28d25a8efd4fb51d7863ae7d1bfd1fe4fb7b1
supaakorn <supaakorn.r@gmail.com>
2023-02-07 11:07:05 +0700

    footer second edition

--- my-app/components/footer.tsx
+++ my-app/components/footer.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 import styles from "../styles/footer.module.css";
 import { Container, Col, Row } from "react-bootstrap";
 import Image from "next/image";
-import linePic from "../img/line.png";
-import instragramPic from "../img/instragram.png";
-import facebookPic from "../img/facebook.png";
-import phonePic from "../img/phone.png";
+import linePic from "../public/line.png";
+import instragramPic from "../public/instragram.png";
+import facebookPic from "../public/facebook.png";
+import phonePic from "../public/phone.png";
 
 export default function Footer() {
   return (
@@ -17,19 +17,32 @@ export default function Footer() {
               <h4>
                 <b>ศูนย์ความช่วยเหลือ</b>
               </h4>
+              <div className={styles.footer_info}>
+                <ul style={{ paddingLeft: "20px" }}>
+                  <li>
+                    <Link href="/" className={`${styles.link}`}>
+                      <small>สมัครอย่างไร</small>
+                    </Link>
+                  </li>
+                </ul>
 
-              <Link href="/" className={`${styles.link}`}>
-                <small>สมัครอย่างไร</small>
-              </Link>
-              <br />
-              <Link href="/" className={`${styles.link}`}>
-                <small>ผู้ปล่อยเช่าต้องเตรียมอะไรบ้าง</small>
-              </Link>
-              <br />
-              <Link href="/" className={`${styles.link}`}>
-                <small>ช่องทางการชำระเงินใน Vehicle4U</small>
-              </Link>
-              <br />
+                <ul style={{ paddingLeft: "20px" }}>
+                  <li>
+                    <Link href="/" className={`${styles.link}`}>
+                      <small>ผู้ปล่อยเช่าต้องเตรียมอะไรบ้าง</small>
+                    </Link>
+                  </li>
+                </ul>
+
+                <ul style={{ paddingLeft: "20px" }}>
+                  <li>
+                    <Link href="/about" className={`${styles.link}`}>
+                      <small>ช่องทางการชำระเงินใน Vehicle4U</small>
+                    </Link>
+                  </li>
+                </ul>
+                <br />
+              </div>
             </div>
           </Col>
           <Col sm={12} lg={4} style={{ borderRight: "1px solid black" }}>
@@ -39,64 +52,73 @@ export default function Footer() {
                 <font color="545a8b">VEHICLE4U</font>
               </b>
             </h4>
-            <ul style={{ paddingLeft: "20px" }}>
-              <li>
-                <Link href="/about" className={`${styles.link}`}>
-                  <small>เกี่ยวกับเรา</small>
-                </Link>
-              </li>
-            </ul>
+            <div className={styles.footer_info}>
+              <ul style={{ paddingLeft: "20px" }}>
+                <li>
+                  <Link href="/about" className={`${styles.link}`}>
+                    <small>เกี่ยวกับเรา</small>
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </Col>
           <Col sm={12} lg={4}>
             <h4>
               <b>ติดต่อเรา</b>
             </h4>
-            <ul style={{ listStyleType: "none" }} className="p-0">
-              <li>
-                <small>
-                  <Image
-                    src={phonePic}
-                    alt="Picture of the author"
-                    width={30}
-                    height={30}
-                    priority
-                  />
-                  : 081-831-8928
-                </small>
-                <br />
-                <br />
-                <div>
-                  <a href="/">
+            <div className={styles.footer_info}>
+              <ul style={{ listStyleType: "none" }} className="p-0">
+                <li>
+                  <small>
                     <Image
-                      src={facebookPic}
+                      className="me-2"
+                      src={phonePic}
                       alt="Picture of the author"
-                      width={42}
-                      height={42}
+                      width={30}
+                      height={30}
                       priority
                     />
-                  </a>
-                  <a href="/">
-                    <Image
-                      src={instragramPic}
-                      alt="Picture of the author"
-                      width={40}
-                      height={40}
-                      priority
-                    />
-                  </a>
+                    : 081-831-8928
+                  </small>
 
-                  <a className="pl-4" href="/">
-                    <Image
-                      src={linePic}
-                      alt="Picture of the author"
-                      width={43}
-                      height={43}
-                      priority
-                    />
-                  </a>
-                </div>
-              </li>
-            </ul>
+                  <br />
+
+                  <div className="mt-3">
+                    <a href="/">
+                      <Image
+                        className="me-3"
+                        src={facebookPic}
+                        alt="Picture of the author"
+                        width={42}
+                        height={42}
+                        priority
+                      />
+                    </a>
+                    <a href="/">
+                      <Image
+                        className="me-3"
+                        src={instragramPic}
+                        alt="Picture of the author"
+                        width={40}
+                        height={40}
+                        priority
+                      />
+                    </a>
+
+                    <a href="/">
+                      <Image
+                        className="me-3"
+                        src={linePic}
+                        alt="Picture of the author"
+                        width={43}
+                        height={43}
+                        priority
+                      />
+                    </a>
+                  </div>
+                </li>
+              </ul>
+            </div>
           </Col>
         </Row>
       </Container>
