d253c3567f647a361f54c809d8f09c683bb121e6
Tanasak Pusawatwong <tanasak32123@gmail.com>
2023-02-08 18:29:12 +0700

    fix nav

--- my-app/components/navbar.tsx
+++ my-app/components/navbar.tsx
@@ -3,72 +3,70 @@ import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import styles from "../styles/components/navbar.module.css";
 
 export default function Header() {
-
-
-  const [login,setLogin] = useState("register");
+  const [login, setLogin] = useState("register");
 
   const userLogout = () => {
     sessionStorage.removeItem("status_login");
   };
 
   const userLogin = () => {
-    sessionStorage.setItem("status_login","login");
+    sessionStorage.setItem("status_login", "login");
     // console.log(sessionStorage.getItem("status_login"));
   };
 
   useEffect(() => {
     var status = sessionStorage.getItem("status_login");
-    if ( status == null || status == 'register'){
+    if (status == null || status == "register") {
       status = "register";
     } else {
       status = "login";
     }
-    sessionStorage.setItem("status_login",status);
+    sessionStorage.setItem("status_login", status);
     setLogin(status);
-  },[])
-
+  }, []);
 
   return (
     <>
-    <Navbar collapseOnSelect expand="lg" className={styles.nav}>
-      <Container>
-        <Navbar.Brand className={`px-3 ${styles.brand}`} href="/">
-          VEHICLE4U
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#search">ค้นหายานพาหนะ</Nav.Link>
-            <Nav.Link href="/about">เกี่ยวกับเรา</Nav.Link>
-          </Nav>
+      <Navbar collapseOnSelect expand="lg" className={styles.nav}>
+        <Container>
+          <Navbar.Brand className={`px-3 ${styles.brand}`} href="/">
+            VEHICLE4U
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#search">ค้นหายานพาหนะ</Nav.Link>
+              <Nav.Link href="/about">เกี่ยวกับเรา</Nav.Link>
+            </Nav>
 
-        <Nav className="me-auto">
-          <Nav.Link href="#search">ค้นหายานพาหนะ</Nav.Link>
-          <Nav.Link href="#contact">เกี่ยวกับเรา</Nav.Link>
-        </Nav>
-
-        <Nav>
-          {login == 'register' && (<>
-            <Nav.Link href="/role_selection" onClick={userLogin}>เข้าสู่ระบบ</Nav.Link>
-            <Nav.Link href="#register" className={`${styles.signup}`}>สมัครสมาชิก</Nav.Link>
-          </>)}
-          {login == 'login' && (<>
-            <NavDropdown title="โปรไฟล์" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.2">
-                โปรไฟล์ของฉัน
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="/" onClick={userLogout}>
-                ล็อกเอาท์
-              </NavDropdown.Item>
-            </NavDropdown>
-          </>)}
-        </Nav>
-      
-
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+            <Nav>
+              {login == "register" && (
+                <>
+                  <Nav.Link href="/role_selection" onClick={userLogin}>
+                    เข้าสู่ระบบ
+                  </Nav.Link>
+                  <Nav.Link href="#register" className={`${styles.signup}`}>
+                    สมัครสมาชิก
+                  </Nav.Link>
+                </>
+              )}
+              {login == "login" && (
+                <>
+                  <NavDropdown title="โปรไฟล์" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.2">
+                      โปรไฟล์ของฉัน
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="/" onClick={userLogout}>
+                      ล็อกเอาท์
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
