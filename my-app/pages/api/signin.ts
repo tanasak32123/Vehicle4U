3beeb69970e54117e7eca6264bea6a485a3457f4
Tanasak Pusawatwong <tanasak32123@gmail.com>
2023-02-10 15:55:50 +0700

    cookies-next

--- my-app/pages/api/signin.ts
+++ my-app/pages/api/signin.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { setCookie } from "cookies-next";
 
 export default async function handler(
   req: NextApiRequest,
