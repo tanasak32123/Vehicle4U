4fe7be396e501b6a70e80b6f608b1791fa9cd85d
boom <boomtanut@gmail.com>
2023-02-13 00:24:15 +0700

    add config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,29 +2,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { UserModule} from './user/user.module'
-import { ConfigModule } from '@nestjs/config'
-import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import  config  from './config/config';
 import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env'
-    }),
+      isGlobal : true,
+      load : [config]
+    }) ,
     TypeOrmModule.forRootAsync({
-      useFactory: async (): Promise<TypeOrmModuleOptions> => {
-        return {
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database : process.env.POSTGRES_DB,
-      entities: [join(__dirname, '**/*.entity.{ts,js}')],
-      synchronize: true,
-        };
-      },
+      
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        port: configService.get<number>('database.port'),
+        username: configService.get<string>('database.username'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.name'),
+        host : configService.get<string>('database.host'),
+        synchronize: true,
+        autoLoadEntities: true,
+     }),
+     inject: [ConfigService],
     }),
     UserModule,
     AuthModule,
---
+++ backend/src/config/config.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import * as dotenv from 'dotenv';
+export default () => ({
+  port: parseInt(process.env.PORT, 10) || 3000,
+  database: {
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    name: process.env.DATABASE_NAME,
+  },
+});
