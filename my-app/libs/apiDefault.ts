2ac8e43730e6b986c86209e48e36c724adcf2cf1
Tanasak Pusawatwong <tanasak32123@gmail.com>
2023-02-13 16:09:46 +0700

    add more headers Options

--- my-app/libs/apiDefault.ts
+++ my-app/libs/apiDefault.ts
@@ -3,6 +3,7 @@ import getToken from "./getToken";
 const defaultOptions = {
   headers: {
     Authentication: `Bearer ${getToken}`,
+    Accept: "application/json",
     "Content-Type": "application/json",
   },
 };
