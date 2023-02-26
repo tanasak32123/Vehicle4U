47a7df30f1d85b71bb8491539255c126974693fc
Tanasak Pusawatwong <tanasak32123@gmail.com>
2023-02-26 20:59:00 +0700

    add middleware

--- my-app/middleware.ts
+++ my-app/middleware.ts
@@ -2,18 +2,11 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname.startsWith("/")) {
-    return NextResponse.rewrite(new URL("/index", req.url));
-  }
-  if (req.nextUrl.pathname.startsWith("/profile")) {
-    return NextResponse.rewrite(new URL("/userProfile", req.url));
-  }
+  console.log("Middleware");
 }
 
 export const config = {
   matcher: [
-    "/profile",
-    "/",
     /*
      * Match all request paths except for the ones starting with:
      * - api (API routes)
