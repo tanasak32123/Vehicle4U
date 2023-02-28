c09d9ad26858473a0ca249d02e12667f789b5ef5
shutter <6331335921@student.chula.ac.th>
2023-02-28 15:44:16 +0700

    shutter

--- backend/src/request/request.module.ts
+++ backend/src/request/request.module.ts
@@ -1,8 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserVehicle } from 'src/user/entities/user-vehicle.entity';
-import { User } from 'src/user/entities/user.entity';
-import { Vehicle } from 'src/vehicle/entities/vehicle.entity';
 import { RequestController } from './request.controller';
 import { RequestService } from './request.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
