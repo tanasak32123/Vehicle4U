fbd11bcf3ddc01404d286569b8163f77f5b6814e
shutter <6331335921@student.chula.ac.th>
2023-03-01 20:33:16 +0700

    Initialize update car page

--- backend/src/request/request.module.ts
+++ backend/src/request/request.module.ts
@@ -5,10 +5,6 @@ import { User } from 'src/user/entities/user.entity';
 import { Vehicle } from 'src/vehicle/entities/vehicle.entity';
 import { RequestController } from './request.controller';
 import { RequestService } from './request.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'discord.js';
-import { UserVehicle } from 'src/user/entities/user-vehicle.entity';
-import { Vehicle } from 'src/vehicle/entities/vehicle.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, UserVehicle, Request, Vehicle])],
