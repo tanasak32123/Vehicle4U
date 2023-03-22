db9ce4f26d076763c8750a2eda47c7b7ae16873c
Tanasak Pusawatwong <tanasak32123@gmail.com>
2023-03-22 15:02:56 +0700

    rename import component

--- my-app/pages/profile/index.tsx
+++ my-app/pages/profile/index.tsx
@@ -10,7 +10,7 @@ import { FaArrowAltCircleLeft, FaCheckCircle, FaUserAlt } from "react-icons/fa";
 import Skeleton from "react-loading-skeleton";
 import { Alert } from "react-bootstrap";
 
-import { useAuth } from "@/components/authContext";
+import { useAuth } from "@/components/AuthContext";
 
 const InputForm = dynamic(() => import("@/components/profile/profileForm"), {
   loading: () => <p>Loading...</p>,
