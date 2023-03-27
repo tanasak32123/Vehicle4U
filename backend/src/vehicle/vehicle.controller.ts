016de5d1b2493193341ba7918cb0ecb3ce075be4
RefinXD <The32069@gmail.com>
2023-03-27 10:30:21 +0700

    fixed pagination

--- backend/src/vehicle/vehicle.controller.ts
+++ backend/src/vehicle/vehicle.controller.ts
@@ -1,6 +1,5 @@
 import { Body, Controller, Get, Response, Query, Post } from '@nestjs/common';
 
-
 import { VehicleService } from './vehicle.service';
 import { Vehicle } from './entities/vehicle.entity';
 
@@ -8,21 +7,23 @@ import { Vehicle } from './entities/vehicle.entity';
 export class VehicleController {
   constructor(private readonly vehicleService: VehicleService) {}
 
-
-
   @Get('search')
   async search(
     @Query('province') province: string,
     @Query('name') carName: string,
     @Query('maxPassenger') maxPassenger: number,
+    @Query('page') pageNumber: number,
     @Response() res,
   ) {
     //const pagination_count = 2;
-    const vehicles = await this.vehicleService.findByFilter(
+    const temp = await this.vehicleService.findByFilter(
       province,
       carName,
       maxPassenger,
+      pageNumber,
     );
+    const vehicles = temp[0];
+    const pagination_data = temp[1];
     if (!vehicles) {
       return res.status(404).send({
         statusCode: 404,
@@ -30,6 +31,10 @@ export class VehicleController {
       });
     }
     //console.log(vehicles);
-    return res.status(200).send(vehicles);
+    return res.status(200).json({
+      vehicles: vehicles,
+      page_count: pagination_data.page_count,
+      next_page: pagination_data.next.pageNumber,
+    });
   }
 }
