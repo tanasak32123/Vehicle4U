870222e41189b6de718275f93713d9f613872cbb
RefinXD <The32069@hotmail.com>
2023-04-13 15:52:14 +0700

    added missing module imports

--- backend/src/comments/comments.module.ts
+++ backend/src/comments/comments.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RentingRequest } from 'src/renting-request/entities/renting-request.entity';
+import { User } from 'src/user/entities/user.entity';
+import { Vehicle } from 'src/vehicle/entities/vehicle.entity';
+import { Reply } from 'src/replies/entities/reply.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([RentingRequest, User, Vehicle,Reply])],
   controllers: [CommentsController],
   providers: [CommentsService]
 })
--- backend/src/renting-request/renting-request.module.ts
+++ backend/src/renting-request/renting-request.module.ts
@@ -5,10 +5,11 @@ import { Vehicle } from 'src/vehicle/entities/vehicle.entity';
 import { RentingRequest } from './entities/renting-request.entity';
 import { RentingRequestController } from './renting-request.controller';
 import { RentingRequestService } from './renting-request.service';
+import { Comment } from 'src/comments/entities/comment.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([RentingRequest, User, Vehicle])],
+    TypeOrmModule.forFeature([RentingRequest, User, Vehicle,Comment])],
   controllers: [RentingRequestController],
   providers: [RentingRequestService]
 })
