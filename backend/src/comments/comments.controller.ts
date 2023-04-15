8f2d67135053e3821e34443e3f92ca2a3e466de9
RefinXD <The32069@hotmail.com>
2023-04-15 22:11:23 +0700

    added comment creation function

--- backend/src/comments/comments.controller.ts
+++ backend/src/comments/comments.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete,Response } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
@@ -8,8 +8,18 @@ export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
   @Post()
-  create(@Body() createCommentDto: CreateCommentDto) {
-    return this.commentsService.create(createCommentDto);
+  async create(@Body() createCommentDto: CreateCommentDto,@Response() res) {
+    const comment = await this.commentsService.create(createCommentDto);
+    if (comment != null) {
+      return res.status(200).send({
+        comment: comment
+      })
+    }
+    else {
+      return res.status(500).send({
+        message:"error"
+      })
+    }
   }
 
   @Get()
--- backend/src/comments/comments.service.ts
+++ backend/src/comments/comments.service.ts
@@ -1,11 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Comment } from './entities/comment.entity';
+import { Vehicle } from 'src/vehicle/entities/vehicle.entity';
+import { User } from 'src/user/entities/user.entity';
+import { RentingRequest } from 'src/renting-request/entities/renting-request.entity';
 
 @Injectable()
 export class CommentsService {
-  create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+  constructor(
+    @InjectRepository(Comment)
+    private commentRepository: Repository<Comment>,
+    @InjectRepository(Vehicle)
+    private vehicleRepository: Repository<Vehicle>,
+    @InjectRepository(Vehicle)
+    private requestRepository: Repository<RentingRequest>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+  ) { }
+  
+
+  async create(createCommentDto: CreateCommentDto) {
+    const comment = this.commentRepository.create(createCommentDto)
+    const rentReq = await this.requestRepository.findOneBy({ id: createCommentDto.request_id })
+    comment.user = rentReq.user
+    comment.vehicle = rentReq.vehicle
+    comment.request = rentReq
+    return this.vehicleRepository.save(comment);
   }
 
   findAll() {
