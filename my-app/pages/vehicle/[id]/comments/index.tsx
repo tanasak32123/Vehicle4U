ea6d9462ef44d6a8a6399d9fa10d34b672fb23c9
Tanasak Pusawatwong <tanasak32123@gmail.com>
2023-04-17 23:49:52 +0700

    change route path

--- my-app/pages/vehicle/[id]/comments/index.tsx
+++ my-app/pages/vehicle/[id]/comments/index.tsx
@@ -172,15 +172,17 @@ const Comments: NextPage = () => {
 
               {!commentsLoading && (
                 <>
-                  {commentsData.length <= 0 && (
-                    <div
-                      className={`d-flex justify-content-center align-items-center ${styles.comments}`}
-                    >
-                      ยังไม่มีรีวิว ณ ตอนนี้
-                    </div>
-                  )}
+                  {!commentsData ||
+                    (commentsData.length <= 0 && (
+                      <div
+                        className={`d-flex justify-content-center align-items-center ${styles.comments}`}
+                      >
+                        ยังไม่มีรีวิว ณ ตอนนี้
+                      </div>
+                    ))}
 
-                  {commentsData.length > 0 &&
+                  {commentsData &&
+                    commentsData.length > 0 &&
                     commentsData.map((obj: any) => (
                       <Card
                         id={`review_${obj?.comment.id}`}
