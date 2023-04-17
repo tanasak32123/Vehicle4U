213f7667e786bf7cb39f473c154b167793c6119d
RefinXD <The32069@hotmail.com>
2023-04-17 17:48:14 +0700

    changed route paths fix

--- backend/src/comments/comments.controller.ts
+++ backend/src/comments/comments.controller.ts
@@ -4,7 +4,7 @@ import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 import { ApiTags } from '@nestjs/swagger';
 @ApiTags('comments')
-@Controller('')
+@Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
