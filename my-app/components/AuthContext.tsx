3ad0007e3bbf5c91d412c585e239b784427e38a6
boom <boomtanut@gmail.com>
2023-04-26 19:26:38 +0700

    change file name

---
+++ my-app/components/AuthContext.tsx
@@ -0,0 +1,49 @@
+import { createContext, useContext, useState } from "react";
+import useSWR, { useSWRConfig } from "swr";
+import { useRouter } from "next/navigation";
+
+//types
+import AuthContextValue from "types/AuthContextValue";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+const initValues: AuthContextValue = {
+  auth: { status: "SIGNED_OUT", user: null, role: null },
+  isLogout: false,
+  isLoading: true,
+  authAction: {},
+};
+
+const AuthContext = createContext(initValues);
+
+export const AuthProvider = (props: any) => {
+  const { data, isLoading, mutate } = useSWR("/api/auth/fetchuser", fetcher);
+  const [isLogout, setIsLogout] = useState(false);
+  const router = useRouter();
+
+  const logout = async () => {
+    await fetch("/api/auth/logout")
+      .then(() => {
+        router.push("/");
+      })
+      .then(() => {
+        mutate();
+      });
+  };
+
+  const value: AuthContextValue = {
+    auth: data,
+    isLogout,
+    isLoading,
+    authAction: {
+      logout,
+      mutate,
+      setIsLogout,
+    },
+  };
+
+  return <AuthContext.Provider value={value} {...props} />;
+};
+
+export const useAuth = () => useContext(AuthContext);
+export const AuthConsumer = AuthContext.Consumer;
